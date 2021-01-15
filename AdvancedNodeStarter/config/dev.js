b9a149f5061f23c99ffd73d4fd8403a2cfa93303
Arpit Arora <arpit.arora123@gmail.com>
2021-01-15 12:41:02 +0530

    AdvancedNodeStarter project code.

---
+++ AdvancedNodeStarter/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '1051614064792-5u19k8v1gfpnrkih8qv4k5gda13d4ifl.apps.googleusercontent.com',
+  googleClientSecret: 'gpxyNH0GeqG9ewMv3UNT79BZ',
+  mongoURI: 'mongodb://arpit:arpit%401234@ds161520.mlab.com:61520/blog_dev',
+  cookieKey: '123123123'
+};
